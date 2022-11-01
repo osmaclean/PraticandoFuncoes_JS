167b210ab0cde6a1aa8847a20df553e978d0c4bb
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-01 10:22:56 -0300

    Primeira tarefa realizada com sucesso!

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// Primeira Tarefa
+
+
+function leitura() {
+    let numeroInteiro = parseInt(prompt("Insira um número inteiro"));
+    alert(`O seu número inteiro é ${numeroInteiro}`)
+}
+leitura();
+
+// Segunda Tarefa

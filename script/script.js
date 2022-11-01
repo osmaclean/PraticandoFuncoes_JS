d0384adc8914de1ace9c11629de44c5811be12fb
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-01 10:33:17 -0300

    Segunda tarefa realizada com sucesso!

--- script/script.js
+++ script/script.js
@@ -8,3 +8,14 @@ function leitura() {
 leitura();
 
 // Segunda Tarefa
+
+function tresNumeros() {
+    let primeiroNumero = parseInt(prompt("Insira o 1º número"));
+    let segundoNumero = parseInt(prompt("Insira o 2º número"));
+    let terceiroNumero = parseInt(prompt("Insira o 3º número"));
+    let somaDosNumeros = primeiroNumero + segundoNumero + terceiroNumero;
+    alert(`A soma dos números resulta em: ${somaDosNumeros}`)
+}
+tresNumeros();
+
+// Terceira Tarefa
